8b5847c16c4d53aeab1262c1da82fe5988af0159
AksaRose <aksarose3@gmail.com>
2024-05-30 23:33:49 +0530

    build : added DOM

--- app.js
+++ app.js
@@ -1,61 +1,95 @@
-const button1 = document.querySelector("#button1");
-const page1 = document.querySelector(".page1");
-const page2 = document.querySelector(".page2");
-const pg1button1 = document.querySelector("#pg1button1");
-const pg1button2 = document.querySelector("#pg1button2");
-const pg1bt = document.querySelector(".pg1bt");
-const page3 = document.querySelector(".page3");
-const pg2button1 = document.querySelector("#pg2button1");
-const pg2button2 = document.querySelector("#pg2button2");
-const pg2bt = document.querySelector(".pg2bt");
-const page4 = document.querySelector(".page4");
-const pg3button1 = document.querySelector("#pg3button1");
-const pg3button2 = document.querySelector("#pg3button2");
-const pg3bt = document.querySelector(".pg3bt");
+document.addEventListener('DOMContentLoaded', () => {
+const heading = document.getElementById("heading");
+const description = document.getElementById("description")
 
-page2.style.display = 'none'
-page3.style.display = 'none'
-page4.style.display = 'none'
-
+questions = [{question : "Morning or Evening",
+              Options : ["Morning","Evening"],
+              answer : "Evening"
+            },
+             {question : "Cat or Dog",
+              Options : ["Cat", "Dog"],
+              answer : "Dog"
+             },
+             {question : "Road trips or Air travel",
+              Options : ["Road trips", "Air travel"],
+              answer : "Road trips"
+              
+             },
+             {question : "Tokyo or Paris",
+             Options : ["Tokyo", "Paris"],
+             answer : "Tokyo"
+             },
+             {question : "Horror or Comedy",
+             Options : ["Horror", "Comedy"],
+             answer : "Comedy"
+             },
+             {question : "Sweet or Savoury",
+             Options : ["Sweet", "Savoury"],
+             answer : "Sweet"
+             },
+             {question : "Text or Call",
+             Options : ["Text", "Call"],
+             answer : "Call"
+             },
+             {question : "Movies or Book",
+             Options : ["Movie", "Book"],
+             answer : "Book"
+             },
+             {question : "Camping or Staycation",
+             Options : ["Camping", "Staycation"],
+             answer : "Staycation"
+             }
+             
+]
+let currentQuestionIndex = 0;
 let mark = 0;
 
-function sucess2() {
-    mark += 1;
-    page2.style.display = 'none'
-    page3.style.display = 'block'
-    pg2button2.addEventListener("click",fail3);
-    pg2button1.addEventListener("click",sucess3);
-}
-function fail2() {
-    page2.style.display = 'none'
-    page3.style.display = 'block'
-    pg2button2.addEventListener("click",fail3);
-    pg2button1.addEventListener("click",sucess3); 
-}
+function Start () {
+    
+    heading.textContent = "Smash or Pass";
+    const para = document.createElement('p');
+    para.textContent = "you ready??";
+    const button = document.createElement('button');
+    button.textContent= "lets goooo";
 
-function sucess3() {
-    mark += 1;
-    page3.style.display = 'none'
-    page4.style.display = 'block'
-    pg3button2.addEventListener("click",fail4);
-    pg3button1.addEventListener("click",sucess4);
-}
-function fail3() {
-    page3.style.display = 'none'
-    page4.style.display = 'block'
-    pg3button2.addEventListener("click",fail4);
-    pg3button1.addEventListener("click",sucess4); 
-}
+    description.appendChild(para);
+    description.appendChild(button);
 
+    button.addEventListener('click',loadques);
 
+    function loadques() {
+        if(currentQuestionIndex < questions.length) {
+            const currentQuestion = questions[currentQuestionIndex];
+            description.innerHTML = '';
 
-function changefirst() {
-    page1.style.display = 'none'
-    page2.style.display = 'block'
-    pg1button2.addEventListener("click",sucess2);
-    pg1button1.addEventListener("click",fail2);
-}
+            const questCont = document.createElement('div');
+            questCont.className = 'QuestionCont';
+            questCont.textContent = currentQuestion.question;
+            description.appendChild(questCont);
 
-button1.addEventListener("click",changefirst)
+            const optCont = document.createElement('div');
+            optCont.className = 'optionCont';
+            currentQuestion.Options.forEach (option => {
+                const butt = document.createElement('button');
+                butt.textContent= option;
+                optCont.appendChild(butt);
+                butt.addEventListener('click',() => {
+                    if (option=== currentQuestion.answer){
+                        mark = mark + 1;
+                    }
+                    currentQuestionIndex++;
+                    loadques();
+                });
 
+            });
+            description.appendChild(optCont);
+        }
+        else{
+            description.textContent = "Finished";
+            description.textContent = `your score is ${mark}`
+        }
+    }
+};
+Start();
 
+})
